4bad7752b8b2b976d8e3117664eef7355d69107b
Alexander Thiemann <mail@athiemann.net>
2016-09-09 01:35:31 +0200

    add monoid and function timing function

--- src/Data/Time/TimeSpan.hs
+++ src/Data/Time/TimeSpan.hs
@@ -1,20 +1,27 @@
+{-# LANGUAGE BangPatterns #-}
 module Data.Time.TimeSpan
   ( TimeSpan
   , milliseconds, seconds, minutes, hours, days
   , toMilliseconds, toSeconds, toMinutes, toHours, toDays
   , diffUTCTimeTS, addUTCTimeTS
   , sleepTS
+  , timeAction
   )
 where
 
 import Control.Concurrent
 import Data.Time
+import System.CPUTime
 
 -- | An abstract timespan. Use the provided smart constructors to create
 -- a meaningful timespan
 newtype TimeSpan
   = TimeSpan { unTimeSpan :: Double } -- as milliseconds
   deriving (Eq, Ord)
+
+instance Monoid TimeSpan where
+    mempty = TimeSpan 0
+    mappend (TimeSpan a) (TimeSpan b) = TimeSpan (a + b)
 
 milliseconds :: Double -> TimeSpan
 milliseconds = TimeSpan
@@ -54,3 +61,11 @@ addUTCTimeTS a b = addUTCTime (fromRational $ toRational $ toSeconds a) b
 
 sleepTS :: TimeSpan -> IO ()
 sleepTS ts = threadDelay (round $ toMilliseconds ts * 1000)
+
+timeAction :: IO a -> IO (TimeSpan, a)
+timeAction action =
+    do !t1 <- getCPUTime
+       !a <- action
+       !t2 <- getCPUTime
+       let secs = fromIntegral (t2-t1) * 1e-12
+       return (seconds secs, a)
